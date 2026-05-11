a91da227ccb73a4e7c3f7f3c3746f875a0441c7f
Sheimilner <58496336+Sheimilner@users.noreply.github.com>
2026-05-11 16:16:44 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,11 @@
+// vite.config.js
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  base: './', // Esto asegura que las rutas funcionen sin importar dónde se aloje
+})import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
