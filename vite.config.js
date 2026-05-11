a8a26c4454f9150b9c6b0f6d32c91da4231ea35c
Sheimilner <58496336+Sheimilner@users.noreply.github.com>
2026-05-11 16:31:14 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './'
+    base: './board-games/', // Cambia 'board-games' por el nombre exacto de tu repositorio
+})
 })
